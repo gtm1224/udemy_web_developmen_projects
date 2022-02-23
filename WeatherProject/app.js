24dbad92a0b848bfca5a2cebfb6ec7f95a0adffa
Tim Guo <gtm1224@berkeley.edu>
2022-02-23 12:53:00 -0800

    tested api

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,9 +7,20 @@ const https = require("https");
 app.get("/",function(req,res){
   const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=061a58bb2f51b64cfb9a5dc822b0f641&units=metric"
   https.get(url,function(response){
-    console.log(response.main);
+    console.log(response.statusCode)
+    response.on("data",function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon =weatherData.weather[0].icon
+      const imageURL= "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+      // console.log(temp,weatherDescription)
+      res.write("<h1>The temperature in London is "+ temp + "degrees Celcius</h1>")
+      res.write("<h1> The weather is currently "+ weatherDescription+"/<h1>")
+      res.write("<img src="+imageURL+">")
+      res.send()
+    })
   })
-  res.send("hello world!")
 })
 
 
