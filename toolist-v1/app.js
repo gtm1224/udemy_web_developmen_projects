fe1472251f6d59396ad49c18c32775355555cc03
Tim Guo <gtm1224@berkeley.edu>
2022-02-25 13:58:17 -0800

    added functionality and fixed style

--- toolist-v1/app.js
+++ toolist-v1/app.js
@@ -1,9 +1,34 @@
+// set up
 const express = require("express");
 const bodyParser = require("body-parser");
 const app =express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine','ejs');
 
+//init variables:
+let items=[];
+
+
+
+//get method:
 app.get("/",function(req,res){
-    res.send("Hello");
+    let today = new Date();
+    let options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+    let day = today.toLocaleDateString("en-US",options)
+    console.log(day);
+    res.render("list",{kindofDay:day,newListItem: items});
+
+});
+
+app.post("/",function(req,res){
+    let item=req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000,function(){
