ffc06a5f183ba75583090b1798ec67a37f3f5493
Tim Guo <gtm1224@berkeley.edu>
2022-02-26 14:52:57 -0800

    add compose and header route

--- Blog Project/app.js
+++ Blog Project/app.js
@@ -27,7 +27,16 @@ app.get("/contact",function(req,res){
   res.render('contact',{contactContent:contactContent});
 });
 
+app.get("/compose",function(req,res){
+  res.render('compose');
+});
 
+app.post("/compose",function(req,res){
+  const post = {
+    title:req.body.postTitle,
+    content:req.body.postBody
+  };
+});
 
 
 
